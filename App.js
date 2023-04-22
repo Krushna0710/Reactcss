ec1580d5dad309605a2a45418c74d8e7f6c5ca1a
Krushna0710 <131531302+Krushna0710@users.noreply.github.com>
2023-04-22 17:43:01 +0530

    React_css_file_added

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import ExpenseItem from './components/ExpenseItem';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+         <h2>Krushnakant shinde</h2>
+         <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
